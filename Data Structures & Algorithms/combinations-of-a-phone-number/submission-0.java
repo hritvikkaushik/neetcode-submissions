f48845607f524160a4405bbc42a49a0357a93165
Hritvik Kaushik <68787274+hritvikkaushik@users.noreply.github.com>
2026-08-04 09:55:27 +0530

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    private static final char[][] MAP = {
+        {},                     // 0
+        {},                     // 1
+        {'a', 'b', 'c'},        // 2
+        {'d', 'e', 'f'},        // 3
+        {'g', 'h', 'i'},        // 4
+        {'j', 'k', 'l'},        // 5
+        {'m', 'n', 'o'},        // 6
+        {'p', 'q', 'r', 's'},   // 7
+        {'t', 'u', 'v'},        // 8
+        {'w', 'x', 'y', 'z'}    // 9
+    };
+
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() == 0) {
+            return new ArrayList<>();
+        }
+        List<String> res = new ArrayList<>();
+        search(digits, 0, "", res);
+        return res;
+    }
+
+    private void search(String digits, int idx, String cur, List<String> res) {
+        if (idx == digits.length()) {
+            res.add(new String(cur));
+            return;
+        }
+        int digit = digits.charAt(idx) - '0';
+        for (char c: MAP[digit]) {
+            search(digits, idx + 1, cur + c, res);
+        }
+    }
+}
